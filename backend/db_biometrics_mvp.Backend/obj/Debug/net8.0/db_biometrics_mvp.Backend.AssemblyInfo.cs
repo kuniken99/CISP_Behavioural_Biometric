1ab9c620556e447a61fe2fa3d5efeca7b3247c31
Kennedy Tan <ktan398@gmail.com>
2025-09-21 20:02:33 +0800

    initial commit

--- backend/db_biometrics_mvp.Backend/obj/Debug/net8.0/db_biometrics_mvp.Backend.AssemblyInfo.cs
+++ backend/db_biometrics_mvp.Backend/obj/Debug/net8.0/db_biometrics_mvp.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("db_biometrics_mvp.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aaee3d33e3aec783e33910eff54fe7dccfa469e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5fa07091a26aeb11f046e3768fd4d6040dbebda")]
 [assembly: System.Reflection.AssemblyProductAttribute("db_biometrics_mvp.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("db_biometrics_mvp.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
