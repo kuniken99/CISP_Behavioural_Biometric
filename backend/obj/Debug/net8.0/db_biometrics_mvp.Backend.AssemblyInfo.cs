46f116cba95cd16df523e3e6b5e9a475c9836d1f
Kennedy Tan <ktan398@gmail.com>
2025-10-11 16:01:52 +0800

    update captcha

--- backend/obj/Debug/net8.0/db_biometrics_mvp.Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/db_biometrics_mvp.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("db_biometrics_mvp.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa05f607903eab51a12e024a2e1b09a1b0c01ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9edbb024392dff679abf2767ac4eaba135913c81")]
 [assembly: System.Reflection.AssemblyProductAttribute("db_biometrics_mvp.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("db_biometrics_mvp.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
