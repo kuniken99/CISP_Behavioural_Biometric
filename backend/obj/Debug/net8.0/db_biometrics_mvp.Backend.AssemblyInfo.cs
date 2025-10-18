f8c1e2b03fae555e0890962cb814288382c32492
Kennedy Tan <ktan398@gmail.com>
2025-10-18 08:31:27 +0800

    backend

--- backend/obj/Debug/net8.0/db_biometrics_mvp.Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/db_biometrics_mvp.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("db_biometrics_mvp.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2db62084d51083971c2b653acd92186811497ca2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46f887ea69af1b6f5ab6b787990b748a3e563daa")]
 [assembly: System.Reflection.AssemblyProductAttribute("db_biometrics_mvp.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("db_biometrics_mvp.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
