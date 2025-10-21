fc8f6cd566ae1fb71c0861f8eaf3e961b1de5eef
Kennedy Tan <ktan398@gmail.com>
2025-10-21 22:23:15 +0800

    db entry management

--- backend/Controllers/DbManagementController.cs
+++ backend/Controllers/DbManagementController.cs
@@ -432,8 +432,8 @@ namespace db_biometrics_mvp.Backend.Controllers
         {
             using (var sha256 = SHA256.Create())
             {
-                var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-                return Convert.ToBase64String(bytes);
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
             }
         }
     }
--- backend/obj/Debug/net8.0/db_biometrics_mvp.Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/db_biometrics_mvp.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("db_biometrics_mvp.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+916a5482de8899ea439e5af75a63fd622092f679")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e51c1a9ef4fa693a8c257294f2b5a27bf99cc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("db_biometrics_mvp.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("db_biometrics_mvp.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
